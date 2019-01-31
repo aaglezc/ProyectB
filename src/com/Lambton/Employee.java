187baa7206e98a19a2689059db838c73acc84497
Arthur Gonzalez <aaglezc@gmail.com>
2019-01-31 00:21:07 -0500

    Main Class

---
+++ src/com/Lambton/Employee.java
@@ -0,0 +1,4 @@
+package com.Lambton;
+
+public class Employee {
+}
--- src/com/Lambton/Main.java
+++ src/com/Lambton/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello Arthur");
+        System.out.println("--Project B");
+        Employee e = new Employee(1,"Arthur G.",47);
+
+        System.out.println(e.calcEarning());
+        System.out.println(e.getId()+" --"+e.getName()+" --"+e.getAge());
 
 
     }
